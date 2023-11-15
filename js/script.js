018dda06117400a209f86d80296afc16aaaff296
ItsKristian01 <kristianhana11@gmail.com>
2023-11-15 17:52:43 +0100

    added dynamically items

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-
+      items: {
+        item1: "Task list and assignments",
+        item2: "Set due date and assignments",
+        item3: "Remove duplicate tasks and stories",
+        item4: "Update the userflow and stories",
+        item5: "Adjust the components",
+      }
     }
   },
+  methods: {
+    
+  }
 }).mount("#app");
