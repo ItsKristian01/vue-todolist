884a243e94675eb4190dc4a7061670fdab5c2dc9
ItsKristian01 <kristianhana11@gmail.com>
2023-11-15 18:21:21 +0100

    update: added array of objects and added remover

--- js/script.js
+++ js/script.js
@@ -3,16 +3,28 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      items: {
-        item1: "Task list and assignments",
-        item2: "Set due date and assignments",
-        item3: "Remove duplicate tasks and stories",
-        item4: "Update the userflow and stories",
-        item5: "Adjust the components",
-      }
-    }
-  },
+      items: [
+        {
+          item: "Task list and assignments",
+        },
+        {
+          item: "Set due date and assignments",
+        },
+        {
+          item: "Remove duplicate tasks and stories",
+        },
+        {
+          item: "Update the userflow and stories",
+        },
+        {
+            item: "Adjust the components",
+        },
+      ],
+     };
+    },
   methods: {
-    
+    removeElement: function (index) {
+        this.items.splice(index, 1);
+      }
   }
 }).mount("#app");
